07a2b6d6d4d7fb521a318f482a9ac62eb944ac0e
Viney <vineykhaneja999@gmail.com>
2022-03-01 08:43:38 +0530

    [Viney] FEATURE ADDED: Founding Word from Binary Search

--- Program.cs
+++ Program.cs
@@ -9,67 +9,24 @@ namespace Algorithm_Programs
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the string: ");
-            //Variables
-            String word = Console.ReadLine();
-            int lengthOfWord = word.Length;
-            //Recursive Permutation
-            RecursivePermutation(word, 0, lengthOfWord - 1);
-            //Iterative Permutaton
-            IterativePermutation(word);
+            List<string> wordsList = new List<string>();
+            wordsList.Add("We, at Bridgelabz provides real time learning experience for all the students, no matter which branch they are from.");
+            string[] words = String.Join(" ", wordsList).Split(' ');
+            Array.Sort(words);
+            Console.Write($"\n-----Enter the word to be searched from the above sentence :- ");
+            string searchedWord = Console.ReadLine();
+            foundOrNot(words,searchedWord);
         }
-        public static void RecursivePermutation(string word, int a, int b) 
+        static void foundOrNot(string[] wordsList, string wordToBeSearched)
         {
-            if (a == b)
+            int result = Array.BinarySearch(wordsList, wordToBeSearched);
+            if (result > 0)
             {
-                Console.WriteLine(word);
+                Console.WriteLine($"{wordToBeSearched} is found & its index is {result}");
             }
             else
             {
-                for (int i = a; i <= b; i++)
-                {
-                    word = Swapping(word, a, i);
-                    RecursivePermutation(word, a + 1, b);
-                    word = Swapping(word, a, i);
-                }
-            }
-        }
-        public static string Swapping(string word, int i , int j)
-        {
-            char temp;
-            char[] charactersOfWord = word.ToCharArray();
-            temp = charactersOfWord[i];
-            charactersOfWord[i] = charactersOfWord[j];
-            charactersOfWord[j] = temp;
-            string str = new string(charactersOfWord);
-            return str;
-        }
-        public static int CalculatingFactorial(int lengthOfWord)
-        {
-            int factorial = 1;
-            for (int i = 1; i <= lengthOfWord; i++)
-            {
-                factorial *= i;
-            }
-            return factorial;
-        }
-        public static void IterativePermutation(string word)
-        {
-            int lengthOfWord = word.Length;
-            int factorial = CalculatingFactorial(lengthOfWord);
-            for (int i = 0; i < factorial; i++)
-            {
-                string originalWord = word;
-                int temp = i;
-                for (int j = word.Length; j >= 1; j--)
-                {
-                    int quotient = temp / j;
-                    int reminder = temp % j;
-                    Console.Write(originalWord[reminder]);
-                    originalWord = originalWord.Remove(reminder, 1);
-                    temp = quotient;
-                }
-                Console.WriteLine();
+                Console.WriteLine($"{wordToBeSearched} is not found");
             }
         }
     }
