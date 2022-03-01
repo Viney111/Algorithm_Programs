e7784b003d72b7e8c9176bc97829f89fe74a0dee
Viney <vineykhaneja999@gmail.com>
2022-03-01 19:59:34 +0530

    [Viney] FEATURE ADDED: Prime_Number_Range

---
+++ PrimeNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Programs
+{
+    internal class PrimeNumbers
+    {
+        public static void PrimeNumberRange(int minNumber, int maxNumber)
+        {
+            for(int i = minNumber; i <= maxNumber; i++)
+            {
+                if (i > 2)
+                {
+                    int temp = i;
+                    bool isPrime = true;
+                    for (int j = 2; j < temp; j++)
+                    {
+                        if (temp % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime == true)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+                else
+                {
+                    if( i == 2)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,17 +9,7 @@ namespace Algorithm_Programs
     {
         static void Main(string[] args)
         {
-            string name1 = "heart";
-            string name2 = "earth";
-            char[] charactersOfName1 = name1.ToCharArray();
-            char[] charactersOfName2 = name2.ToCharArray();
-            ArraySorting.BubbleSort<char>(charactersOfName1);
-            ArraySorting.BubbleSort<char>(charactersOfName2);
-            string name1Sorted = new string(charactersOfName1);
-            string name2Sorted = new string(charactersOfName2);
-            string result = name1Sorted == name2Sorted ? $"{name1} & {name2} are anagrams" : $"{name1} & {name2} are not anagrams";
-            Console.WriteLine($"\n{result}");
-
+            PrimeNumbers.PrimeNumberRange(0,1000);
         }
     }
 }
